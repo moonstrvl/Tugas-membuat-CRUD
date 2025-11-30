040fb9501a060d42e05412bfd5659170ae2a6d40
ardhyan15 <98029961+ardhyan15@users.noreply.github.com>
2025-11-30 23:37:36 +0700

    Add files via upload

---
+++ CREATE TABLE barang (.sql
@@ -0,0 +1,9 @@
+CREATE TABLE barang (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    nama_barang VARCHAR(100),
+    kategori VARCHAR(50),
+    harga_jual INT,
+    harga_beli INT,
+    stok INT,
+    gambar VARCHAR(255)
+);
